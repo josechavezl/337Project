a86f7d0793f53654d7895ddb5fee43f758838e93
Jose Santiago Campa Morales <jscm1607@arizona.edu>
2024-12-12 17:05:14 -0700

    created comment schema and backend req

--- server/jsBackend/file.js
+++ server/jsBackend/file.js
@@ -9,7 +9,7 @@ try {
         name: { type: String, required: true, unique: true },
         author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
         date: { type: Date, default: Date.now },
-        folder: [{ type: mongoose.Schema.Types.ObjectId, ref: "Folder" }],
+        folder: { type: mongoose.Schema.Types.ObjectId, ref: "Folder" },
         path: { type: String, required: true }
     });
 
